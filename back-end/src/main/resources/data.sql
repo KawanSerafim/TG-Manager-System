74485f48c060e63316e719068e883aa24cf9a08d
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2025-09-10 20:32:01 -0300

    chore: foi configurado para o spring sempre aceitar um script do sql

--- back-end/src/main/resources/data.sql
+++ back-end/src/main/resources/data.sql
@@ -1,5 +1,6 @@
-INSERT INTO courses (id, name) VALUES (1, 'Engenharia de Software')
-ON DUPLICATE KEY UPDATE name = VALUES(name);
+DELETE FROM students;
+DELETE FROM student_groups;
+DELETE FROM courses;
 
-INSERT INTO student_groups (id, course_id, year, semester) VALUES (1, 1, 2025, 2)
-ON DUPLICATE KEY UPDATE course_id = VALUES(course_id);
+INSERT INTO courses(id, name) VALUES(1, 'Engenharia de Software');
+INSERT INTO student_groups(id, course_id, year, semester) VALUES(1, 1, 2025, 2);
