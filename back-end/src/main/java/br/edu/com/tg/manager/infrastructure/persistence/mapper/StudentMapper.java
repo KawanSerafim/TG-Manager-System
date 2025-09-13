7722104c7d178bd4d713bfe80c4d2fbcdba79633
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2025-09-13 15:24:53 -0300

    docs: comentários adicionados aos mappers da camada de infraestrutura na persistência.

--- back-end/src/main/java/br/edu/com/tg/manager/infrastructure/persistence/mapper/StudentMapper.java
+++ back-end/src/main/java/br/edu/com/tg/manager/infrastructure/persistence/mapper/StudentMapper.java
@@ -4,26 +4,50 @@ import br.edu.com.tg.manager.core.entity.Student;
 import br.edu.com.tg.manager.infrastructure.persistence.model.StudentData;
 
 /**
- * Mapper responsável por converter a entidade de domínio
- * Student para modelo de dados StudentData, e vice-versa.
+ * Mapa que linka a entidade de domínio do aluno com
+ * o seu modelo de dados, formando uma ponte lógica
+ * entre a camada core e a de persistence.
  */
 public class StudentMapper {
 
+    /* Um cadeado, evitando que a classe seja instanciada,
+     * pois ela é puramente de serviço.
+     */
     private StudentMapper(){}
 
+    /**
+     * Método que converte o modelo de dados do aluno
+     * para a sua entidade domínio.
+     * @param data Modelo de dados do aluno.
+     * @return Entidade de domínio do aluno.
+     */
     public static Student toDomain(StudentData data) {
 
+        /* Se não houver nada no modelo de dados,
+         * é retornado um null.
+         */
         if(data == null) {
 
             return null;
         }
 
-        var student = new Student();
+        /* Chama o construtor da entidade de domínio
+         * do aluno, colocando no valor do nome,
+         * matrícula (RA), email e senha, os mesmos 
+         * que estão no modelo de dados.
+         */
+        var student = new Student(
+            data.getName(),
+            data.getRegistration(),
+            data.getEmail(),
+            data.getPassword()
+        );
+
+        /* 
+         * Por estar fora do construtor, o ID e a turma
+         * são via Set. 
+         */
         student.setId(data.getId());
-        student.setName(data.getName());
-        student.setRegistration(data.getRegistration());
-        student.setEmail(data.getEmail());
-        student.setPassword(data.getPassword());
 
         student.setStudentGroup(StudentGroupMapper
         .toDomain(data.getStudentGroup()));
@@ -33,11 +57,18 @@ public class StudentMapper {
 
     public static StudentData toData(Student domain) {
 
+        /* Se não houver nada na entidade de domínio,
+         * é retornado um null.
+         */
         if(domain == null) {
 
             return null;
         }
 
+        /* Como o construtor no modelo de dados é
+         * vazio, os valores são inseridos apenas 
+         * via métodos sets.
+         */
         var studentData = new StudentData();
         studentData.setId(domain.getId());
         studentData.setName(domain.getName());
