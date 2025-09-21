01d7165de183f9a48ef4ea089d6626470d5c5b5f
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2025-09-21 20:20:13 -0300

    feat: adicionado à camada de entidades, um enum para representar os turnos dos cursos.

---
+++ back-end/src/main/java/br/edu/com/tg/manager/core/entities/CourseShift.java
@@ -0,0 +1,12 @@
+package br.edu.com.tg.manager.core.entities;
+
+/**
+ * Enumeração de domínio.
+ * Representa os turnos dos cursos.
+ */
+public enum CourseShift {
+
+    MORNING,
+    AFTERNOON,
+    NIGHT
+}
