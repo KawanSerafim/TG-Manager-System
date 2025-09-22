1883610960c5c3c3dd18618ba2ace2ab0e5d39e4
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2025-09-21 21:14:11 -0300

    docs: foi acrescentado ao comentário da classe, os padrões de texto de exceção de domínio.

--- back-end/src/main/java/br/edu/com/tg/manager/core/exceptions/DomainException.java
+++ back-end/src/main/java/br/edu/com/tg/manager/core/exceptions/DomainException.java
@@ -4,6 +4,34 @@ package br.edu.com.tg.manager.core.exceptions;
  * Exceção especializada.
  * Lançada para indicar uma violação de uma regra de negócio dentro do domínio
  * da aplicação.
+ * 
+ * -------------------
+ * 
+ * Padrões de exceção:
+ * 
+ * -------------------
+ * 
+ * - Campo obrigatório:
+ * 
+ * [O campo 'nome do campo' é obrigatório.]
+ * 
+ * -------------------
+ * 
+ * - Formato inválido:
+ * 
+ * [O formato do campo 'nome do campo' é inválido.]
+ * 
+ * -------------------
+ * 
+ * - Relação obrigatória:
+ * 
+ * ['Entidade A' deve estar associado(a) a um(a) 'Entidade B'.]
+ * 
+ * -------------------
+ * 
+ * - Não encontrado:
+ *
+ * ['Entidade' com 'campo' = 'valor' não foi encontrado(a).]
  */
 public class DomainException extends RuntimeException {
 
