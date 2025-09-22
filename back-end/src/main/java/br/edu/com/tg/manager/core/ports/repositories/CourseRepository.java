e0dc0d3f019d72b765d70192fcea49d3bb603513
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2025-09-22 01:35:26 -0300

    feat: adicionado à camada de portas, na parte de repositórios, uma interface para o repositório de domínio do curso.

---
+++ back-end/src/main/java/br/edu/com/tg/manager/core/ports/repositories/CourseRepository.java
@@ -0,0 +1,25 @@
+package br.edu.com.tg.manager.core.ports.repositories;
+
+import br.edu.com.tg.manager.core.domain.entities.Course;
+import java.util.Optional;
+
+/**
+ * Repositório de domínio.
+ * Determina um contrato abstrato para a camada de infra-estrutura implementar
+ * a persistência de dados da entidade de domínio Course.
+ */
+public interface CourseRepository {
+
+    /**
+     * Persiste o objeto de um curso no banco de dados.
+     * @param course Curso
+     */
+    void save(Course course);
+
+    /**
+     * Busca um curso pelo seu nome.
+     * @param name Nome.
+     * @return Optional vazio ou Course.
+     */
+    Optional<Course> findByName(String name);
+}
