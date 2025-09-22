9efab154287d27e7672ae00c1e82ac9c8aadafcf
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2025-09-22 01:36:29 -0300

    feat: adicionado à camada de portas, na parte de repositórios, uma interface para o repositório de domínio da turma.

---
+++ back-end/src/main/java/br/edu/com/tg/manager/core/ports/repositories/StudentGroupRepository.java
@@ -0,0 +1,33 @@
+package br.edu.com.tg.manager.core.ports.repositories;
+
+import br.edu.com.tg.manager.core.domain.entities.StudentGroup;
+import br.edu.com.tg.manager.core.domain.entities.Course;
+import java.util.Optional;
+
+/**
+ * Repositório de domínio.
+ * Determina um contrato abstrato para a camada de infra-estrutura implementar
+ * a persistência de dados da entidade de domínio StudentGroup.
+ */
+public interface StudentGroupRepository {
+
+    /**
+     * Persiste o objeto de uma turma no banco de dados.
+     * @param studentGroup
+     */
+    void save(StudentGroup studentGroup);
+
+    /**
+     * Busca uma turma pelo seu curso, seu ano e seu semestre.
+     * @param course Curso.
+     * @param year Ano.
+     * @param semester Semestre.
+     * @return Optional vazio ou StudentGroup.
+     */
+    Optional<StudentGroup> findByCourseAndYearAndSemester(
+        
+        Course course,
+        Integer year,
+        Integer semester
+    );
+}
