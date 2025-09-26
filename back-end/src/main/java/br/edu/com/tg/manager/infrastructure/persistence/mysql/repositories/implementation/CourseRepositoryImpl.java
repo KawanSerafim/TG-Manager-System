669221240dbce192e7b0e3c4a34e41ff906d9429
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2025-09-26 11:45:18 -0300

    feat: adicionado à camada de infrastructure, na parte de persistence mysql, na implementation dos repositories, o implementador do repositório da entidade de domínio Course.

---
+++ back-end/src/main/java/br/edu/com/tg/manager/infrastructure/persistence/mysql/repositories/implementation/CourseRepositoryImpl.java
@@ -0,0 +1,52 @@
+package br.edu.com.tg.manager.infrastructure.persistence.mysql.repositories.implementation;
+
+import br.edu.com.tg.manager.core.domain.entities.Course;
+import br.edu.com.tg.manager.core.domain.enums.CourseShift;
+import br.edu.com.tg.manager.core.ports.repositories.CourseRepository;
+import br.edu.com.tg.manager.infrastructure.persistence.mysql.mappers
+.CourseMapper;
+import br.edu.com.tg.manager.infrastructure.persistence.mysql.models
+.CourseModel;
+import br.edu.com.tg.manager.infrastructure.persistence.mysql.repositories
+.SpringCourseRepository;
+import java.util.Optional;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CourseRepositoryImpl implements CourseRepository {
+
+    private final SpringCourseRepository springRepository;
+    private final CourseMapper courseMapper;
+
+    public CourseRepositoryImpl(
+
+        SpringCourseRepository springRepository,
+        CourseMapper courseMapper
+    ) {
+
+        this.springRepository = springRepository;
+        this.courseMapper = courseMapper;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void save(Course course) {
+
+        var courseModel = courseMapper.toModel(course);
+        springRepository.save(courseModel);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Optional<Course> findByNameAndShift(String name, CourseShift shift) {
+
+        Optional<CourseModel> optionalCourseModel = springRepository
+            .findByNameAndShift(name, shift);
+
+        return optionalCourseModel.map(courseMapper::toDomain);
+    }
+}
