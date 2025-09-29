0f1571aec133a4d62db705d54a733c9bc4942a51
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2025-09-28 22:45:46 -0300

    docs: exception da exceção especializada de domínio DomainException foi re-documentada.

--- back-end/src/main/java/br/edu/com/tg/manager/core/domain/exceptions/DomainException.java
+++ back-end/src/main/java/br/edu/com/tg/manager/core/domain/exceptions/DomainException.java
@@ -1,70 +1,71 @@
 package br.edu.com.tg.manager.core.domain.exceptions;
 
 /**
- * Exceção especializada.
- * Lançada para indicar uma violação de uma regra de negócio dentro do domínio
+ * Exceção especializada de domínio:
+ * Representa as violações de uma regra de negócio dentro do domínio
  * da aplicação.
- * 
- * -------------------
- * 
- * Padrões de exceção:
- * 
- * -------------------
- * 
- * - Campo obrigatório:
- * 
- * [O campo 'nome do campo' é obrigatório.]
- * 
- * -------------------
- * 
- * - Formato inválido:
- * 
- * [O formato do campo 'nome do campo' é inválido.]
- * 
- * -------------------
- * 
- * - Relação obrigatória:
- * 
- * [O(A) 'Entidade A' deve estar associado(a) a um(a) 'Entidade B'.]
- * 
- * -------------------
- * 
- * - Não encontrado:
- *
- * [O(A) 'Entidade' com 'campo' = 'valor' não foi encontrado(a).]
- * 
- * -------------------
- * 
- * - Sem permissão:
- * 
- * [O(A) 'Entidade' não tem permissão de 'Nível ou Cargo'.]
- * 
- * -------------------
- * 
- * - Estado inválido:
- * 
- * [O(A) 'Entidade' não tem o estado válido para a ação.]
- * 
- * -------------------
- * 
- * - Diferente do tempo atual:
- * 
- * [O 'tempo' não pode ser diferente do atual.]
- * 
- * -------------------
- * 
- * - Diferente do padrão:
- * 
- * [O 'campo' foge do padrão: 'padrão'.]
  */
 public class DomainException extends RuntimeException {
 
     /**
-     * Construtor que aceita a mensagem de erro.
-     * @param message Mensagem detalhando a violação da regra de negócio.
+     * Construtor de negócio:
+     * Encaminha a mensagem de erro ao método do RuntimeException.
+     * @param message Mensagem de erro.
      */
     public DomainException(String message) {
 
         super(message);
     }
 }
+
+/*
+ * Padrões de texto de exceção:
+ *
+ * -------------------
+ *
+ * - Campo obrigatório:
+ *
+ * [O campo 'nome do campo' é obrigatório.]
+ *
+ * -------------------
+ *
+ * - Formato inválido:
+ *
+ * [O formato do campo 'nome do campo' é inválido.]
+ *
+ * -------------------
+ *
+ * - Relação obrigatória:
+ *
+ * [O(A) 'Entidade A' deve estar associado(a) a um(a) 'Entidade B'.]
+ *
+ * -------------------
+ *
+ * - Não encontrado:
+ *
+ * [O(A) 'Entidade' com 'campo' = 'valor' não foi encontrado(a).]
+ *
+ * -------------------
+ *
+ * - Sem permissão:
+ *
+ * [O(A) 'Entidade' não tem permissão de 'Nível ou Cargo'.]
+ *
+ * -------------------
+ *
+ * - Estado inválido:
+ *
+ * [O(A) 'Entidade' não tem o estado válido para a ação.]
+ *
+ * -------------------
+ *
+ * - Diferente do tempo atual:
+ *
+ * [O 'tempo' não pode ser diferente do atual.]
+ *
+ * -------------------
+ *
+ * - Diferente do padrão:
+ *
+ * [O 'campo' foge do padrão: 'padrão'.]
+ */
