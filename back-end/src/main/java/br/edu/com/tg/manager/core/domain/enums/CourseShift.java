991736e4d08ed084efa92804b7a08f05245bcb32
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2025-09-28 22:27:19 -0300

    docs: enum da enumeração de domínio CourseShift foi re-documentado.

--- back-end/src/main/java/br/edu/com/tg/manager/core/domain/enums/CourseShift.java
+++ back-end/src/main/java/br/edu/com/tg/manager/core/domain/enums/CourseShift.java
@@ -1,12 +1,17 @@
 package br.edu.com.tg.manager.core.domain.enums;
 
 /**
- * Enumeração de domínio.
+ * Enumeração de domínio:
  * Representa os turnos dos cursos.
  */
 public enum CourseShift {
 
+    // Manhã.
     MORNING,
+
+    // Tarde.
     AFTERNOON,
+
+    // Noite.
     NIGHT
 }
