36286ad0a0aafcba2ae95b29aa4f085399b3e70e
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2025-09-28 23:21:42 -0300

    docs: repository do repositório de domínio StudentGroupRepository foi re-documentado.

--- back-end/src/main/java/br/edu/com/tg/manager/core/ports/repositories/StudentGroupRepository.java
+++ back-end/src/main/java/br/edu/com/tg/manager/core/ports/repositories/StudentGroupRepository.java
@@ -5,13 +5,14 @@ import br.edu.com.tg.manager.core.domain.entities.Course;
 import java.util.Optional;
 
 /**
- * Repositório de domínio.
- * Determina um contrato abstrato para a camada de infra-estrutura implementar
+ * Repositório de domínio:
+ * Determina um contrato abstrato para a camada de infraestrutura implementar
  * a persistência de dados da entidade de domínio StudentGroup.
  */
 public interface StudentGroupRepository {
 
     /**
+     * Método de contrato de domínio:
      * Persiste o objeto de uma turma no banco de dados.
      * @param studentGroup StudentGroup.
      * @return StudentGroup.
@@ -19,10 +20,11 @@ public interface StudentGroupRepository {
     StudentGroup save(StudentGroup studentGroup);
 
     /**
+     * Método de contrato de domínio:
      * Busca uma turma pelo seu curso, seu ano e seu semestre.
-     * @param course Curso.
-     * @param year Ano.
-     * @param semester Semestre.
+     * @param course Curso da turma.
+     * @param year Ano da turma.
+     * @param semester Semestre da turma.
      * @return Optional vazio ou StudentGroup.
      */
     Optional<StudentGroup> findByCourseAndYearAndSemester(
