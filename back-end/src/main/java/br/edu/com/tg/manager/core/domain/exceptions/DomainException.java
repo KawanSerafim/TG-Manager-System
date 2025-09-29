b21df4a5cf51cf12f0f2b76d726446c875161e03
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2025-09-29 00:16:46 -0300

    docs: classe da exceção especializada domínio foi re-documentada.

--- back-end/src/main/java/br/edu/com/tg/manager/core/domain/exceptions/DomainException.java
+++ back-end/src/main/java/br/edu/com/tg/manager/core/domain/exceptions/DomainException.java
@@ -4,6 +4,9 @@ package br.edu.com.tg.manager.core.domain.exceptions;
  * Exceção especializada de domínio:
  * Representa as violações de uma regra de negócio dentro do domínio
  * da aplicação.
+ * Por pertencer ao núcleo (core) da aplicação, esta classe é independente de
+ * frameworks ou bibliotecas externas, sendo, portanto, considerada uma classe
+ * pura.
  */
 public class DomainException extends RuntimeException {
 
