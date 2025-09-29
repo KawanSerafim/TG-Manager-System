b05167a794429571f1a1beddd254f261b3d901c6
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2025-09-29 00:22:25 -0300

    docs: interface do caso de uso de domínio CreateStudentGroupCase foi re-documentado.

--- back-end/src/main/java/br/edu/com/tg/manager/core/usecases/CreateStudentGroupCase.java
+++ back-end/src/main/java/br/edu/com/tg/manager/core/usecases/CreateStudentGroupCase.java
@@ -7,6 +7,9 @@ import java.io.InputStream;
  * Caso de uso de domínio:
  * Define um contrato abstrato para a camada de aplicação implementar a
  * lógica de criar uma turma.
+ * Por pertencer ao núcleo (core) da aplicação, esta interface é independente de
+ * frameworks ou bibliotecas externas, sendo, portanto, considerada uma
+ * interface pura.
  */
 public interface CreateStudentGroupCase {
 
