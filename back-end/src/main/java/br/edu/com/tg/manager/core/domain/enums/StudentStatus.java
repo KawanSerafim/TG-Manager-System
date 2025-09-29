a224a56cc45c89cb897b6816c77fdcd4123bbe90
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2025-09-28 22:35:52 -0300

    docs: enum da enumeração de domínio StudentStatus foi re-documentado.

--- back-end/src/main/java/br/edu/com/tg/manager/core/domain/enums/StudentStatus.java
+++ back-end/src/main/java/br/edu/com/tg/manager/core/domain/enums/StudentStatus.java
@@ -1,13 +1,20 @@
 package br.edu.com.tg.manager.core.domain.enums;
 
 /**
- * Enumeração de domínio.
- * Representa os estados de conta dos alunos.
+ * Enumeração de domínio:
+ * Representa os status das contas dos alunos.
  */
 public enum StudentStatus {
 
+    // Pré-Cadastro.
     PRE_REGISTRATION,
+
+    // Verificação de email pendente.
     PENDING_VERIFICATION,
+
+    // Ativo.
     ACTIVE,
+
+    // Inativo.
     INACTIVE
 }
