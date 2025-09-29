c394e391e0069b6d020d78d661a490600c0be3a1
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2025-09-28 23:16:56 -0300

    docs: repository do repositório de domínio ProfessorRepository foi re-documentado.

--- back-end/src/main/java/br/edu/com/tg/manager/core/ports/repositories/ProfessorRepository.java
+++ back-end/src/main/java/br/edu/com/tg/manager/core/ports/repositories/ProfessorRepository.java
@@ -4,21 +4,23 @@ import br.edu.com.tg.manager.core.domain.entities.Professor;
 import java.util.Optional;
 
 /**
- * Repositório de domínio.
- * Determina um contrato abstrato para a camada de infra-estrutura implementar
+ * Repositório de domínio:
+ * Define um contrato abstrato para a camada de infraestrutura implementar
  * a persistência de dados da entidade de domínio Professor.
  */
 public interface ProfessorRepository {
 
     /**
+     * Método de contrato de domínio:
      * Persiste o objeto de um professor no banco de dados.
      * @param professor Professor.
      */
     void save(Professor professor);
 
     /**
+     * Método de contrato de domínio:
      * Busca um professor pela sua matrícula.
-     * @param registration Matrícula.
+     * @param registration Matrícula do professor.
      * @return Optional vazio ou Professor.
      */
     Optional<Professor> findByRegistration(String registration);
