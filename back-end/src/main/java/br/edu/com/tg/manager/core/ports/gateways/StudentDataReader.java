fb43ede9723760adefdda01d63b0b05fe4965a9a
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2025-09-28 23:09:27 -0300

    docs: gateway do portão de acesso de domínio StudentDataReader foi re-documentado.

--- back-end/src/main/java/br/edu/com/tg/manager/core/ports/gateways/StudentDataReader.java
+++ back-end/src/main/java/br/edu/com/tg/manager/core/ports/gateways/StudentDataReader.java
@@ -1,22 +1,19 @@
 package br.edu.com.tg.manager.core.ports.gateways;
 
+import br.edu.com.tg.manager.core.domain.enums.CourseShift;
 import java.io.InputStream;
 import java.util.List;
 
-import br.edu.com.tg.manager.core.domain.enums.CourseShift;
-
 /**
- * Portão de acesso.
+ * Portão de acesso:
  * Define um contrato para a ação de ler dados de alunos e demais informações
- * de turma, vindos de um arquivo.
+ * de turma, todos vindos de um arquivo.
  */
 public interface StudentDataReader {
 
     /**
-     * Porta-dados.
-     * Carrega os dados dos alunos que estão dentro de um arquivo, que são:
-     * - nome do aluno.
-     * - RA do aluno.
+     * Porta-dados:
+     * Carrega os dados dos alunos que estão num arquivo.
      */
     record StudentData(
 
@@ -25,12 +22,9 @@ public interface StudentDataReader {
     ) {}
 
     /**
-     * Porta-dados.
-     * Carrega os demais dados da turma no arquivo, que são:
-     * - ano.
-     * - semestre.
-     * - turno.
-     * Além disso, instancia uma lista de records de alunos.
+     * Porta-dados:
+     * Carrega os demais dados da turma que estão num arquivo. Além disso,
+     * instancia uma lista de records de alunos.
      */
     record FileData(
 
@@ -41,6 +35,7 @@ public interface StudentDataReader {
     ) {}
 
     /**
+     * Método de contrato de domínio:
      * Lê um fluxo de dados de um arquivo e o converte em FileData.
      * @param fileStream Fluxo de dados do arquivo.
      * @return FileData.
