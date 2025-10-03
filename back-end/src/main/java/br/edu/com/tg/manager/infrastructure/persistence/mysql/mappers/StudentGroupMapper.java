3baf80e4fca469a16d99e3c9232d014644af0231
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2025-10-03 00:07:22 -0300

    docs: classe do mapeador StudentGroupMapper foi re-documentada.

--- back-end/src/main/java/br/edu/com/tg/manager/infrastructure/persistence/mysql/mappers/StudentGroupMapper.java
+++ back-end/src/main/java/br/edu/com/tg/manager/infrastructure/persistence/mysql/mappers/StudentGroupMapper.java
@@ -5,37 +5,48 @@ import br.edu.com.tg.manager.infrastructure.persistence.mysql.models
 .StudentGroupModel;
 import org.springframework.stereotype.Component;
 
+/*
+ * Anotação @Component: indica ao Spring que esta classe deve ser gerenciada
+ * pelo framework.
+ */
+
 /**
- * Mapeador.
+ * Mapeador:
  * Estabelece uma ponte entre a entidade de domínio StudentGroup com o modelo de
  * dados StudentGroupModel.
- * 
- * Anotação @Component: indica ao Spring que essa classe deve ser gerenciada,
- * permitindo que ela possa ser injetada.
  */
 @Component
 public class StudentGroupMapper {
 
     private final CourseMapper courseMapper;
 
+    /**
+     * Construtor de injeção de dependência:
+     * Injeta diretamente a dependência de que, ao criar StudentGroupMapper,
+     * CourseMapper também é criado.
+     * @param courseMapper Mapeador de curso.
+     */
     public StudentGroupMapper(CourseMapper courseMapper) {
 
         this.courseMapper = courseMapper;
     }
 
     /**
-     * Mapeia a entidade de domínio StudentGroup para o modelo de dados
+     * Método de aplicação:
+     * Converte a entidade de domínio StudentGroup para o modelo de dados
      * StudentGroupModel.
      * @param domain Entidade de domínio StudentGroup.
      * @return Modelo de dados StudentGroupModel.
      */
     public StudentGroupModel toModel(StudentGroup domain) {
 
+        // Cláusula de guarda.
         if(domain == null) {
 
             return null;
         }
 
+        // Inserção dos dados da entidade de domínio no modelo de dados.
         var studentGroupModel = new StudentGroupModel();
 
         studentGroupModel.setId(domain.getId());
@@ -48,22 +59,26 @@ public class StudentGroupMapper {
         studentGroupModel.setSemester(domain.getSemester());
         studentGroupModel.setTemporaryPassword(domain.getTemporaryPassword());
 
+        // Retorno do modelo de dados.
         return studentGroupModel;
     }
 
     /**
-     * Mapeia o modelo de dados StudentGroupModel para a entidade de domínio
+     * Método de aplicação:
+     * Converte o modelo de dados StudentGroupModel para a entidade de domínio
      * StudentGroup.
      * @param model Modelo de dados StudentGroupModel.
      * @return Entidade de domínio StudentGroup.
      */
     public StudentGroup toDomain(StudentGroupModel model) {
 
+        // Cláusula de guarda.
         if(model == null) {
 
             return null;
         }
 
+        // Inserção dos dados do modelo de dados na entidade de domínio.
         var studentGroup = new StudentGroup(
 
             courseMapper.toDomain(model.getCourse()),
@@ -75,6 +90,7 @@ public class StudentGroupMapper {
 
         studentGroup.setId(model.getId());
 
+        // Retorno da entidade de domínio.
         return studentGroup;
     }
 }
