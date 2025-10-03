4d1ad4d701d98dada617d81e84783be287fd1050
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2025-10-02 23:59:52 -0300

    docs: classe do mapeador ProfessorMapper foi re-documentada.

--- back-end/src/main/java/br/edu/com/tg/manager/infrastructure/persistence/mysql/mappers/ProfessorMapper.java
+++ back-end/src/main/java/br/edu/com/tg/manager/infrastructure/persistence/mysql/mappers/ProfessorMapper.java
@@ -5,30 +5,35 @@ import br.edu.com.tg.manager.infrastructure.persistence.mysql.models
 .ProfessorModel;
 import org.springframework.stereotype.Component;
 
+/*
+ * Anotação @Component: indica ao Spring que esta classe deve ser gerenciada
+ * pelo framework.
+ */
+
 /**
- * Mapeador.
+ * Mapeador:
  * Estabelece uma ponte entre a entidade de domínio Professor com o modelo de
  * dados ProfessorModel.
- * 
- * Anotação @Component: indica ao Spring que essa classe deve ser gerenciada,
- * permitindo que ela possa ser injetada.
  */
 @Component
 public class ProfessorMapper {
     
     /**
-     * Mapeia a entidade de domínio Professor para o modelo de dados
+     * Método de aplicação:
+     * Converte a entidade de domínio Professor para o modelo de dados
      * ProfessorModel.
      * @param domain Entidade de domínio Professor.
      * @return Modelo de dados ProfessorModel.
      */
     public ProfessorModel toModel(Professor domain) {
 
+        // Cláusula de guarda.
         if(domain == null) {
 
             return null;
         }
 
+        // Inserção dos dados da entidade de domínio no modelo de dados.
         var professorModel = new ProfessorModel();
 
         professorModel.setId(domain.getId());
@@ -38,22 +43,26 @@ public class ProfessorMapper {
         professorModel.setHashedPassword(domain.getHashedPassword());
         professorModel.setRole(domain.getRole());
 
+        // Retorno do modelo de dados.
         return professorModel;
     }
 
     /**
-     * Mapeia o modelo de dados ProfessorModel para a entidade de domínio
+     * Método de aplicação:
+     * Converte o modelo de dados ProfessorModel para a entidade de domínio
      * Professor.
      * @param model Modelo de dados ProfessorModel.
      * @return Entidade de domínio Professor.
      */
     public Professor toDomain(ProfessorModel model) {
 
+        // Cláusula de guarda.
         if(model == null) {
 
             return null;
         }
 
+        // Inserção dos dados do modelo de dados na entidade de domínio.
         var professor = new Professor(
 
             model.getName(),
@@ -65,6 +74,7 @@ public class ProfessorMapper {
 
         professor.setId(model.getId());
 
+        // Retorno da entidade de domínio.
         return professor;
     }
 }
