e42008485de435f37f898917bc6cdffb254464c1
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2025-10-03 01:51:52 -0300

    docs: interface do SpringProfessorRepository foi re-documentada.

--- back-end/src/main/java/br/edu/com/tg/manager/infrastructure/persistence/mysql/repositories/SpringProfessorRepository.java
+++ back-end/src/main/java/br/edu/com/tg/manager/infrastructure/persistence/mysql/repositories/SpringProfessorRepository.java
@@ -1,21 +1,22 @@
 package br.edu.com.tg.manager.infrastructure.persistence.mysql.repositories;
 
 import br.edu.com.tg.manager.infrastructure.persistence.mysql.models
-.ProfessorModel;
+        .ProfessorModel;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
- * Repositório Spring JPA.
- * Determina um contrato abstrato para o Spring JPA persistir e manipular o 
- * modelo de dados ProfessorModel.
+ * Repositório de fronteira:
+ * Define um contrato abstrato para o Spring JPA persistir e manipular o
+ * modelo de dados ProfessorModel. Por pertencer à infraestrutura da aplicação,
+ * esta classe herda o JPARepository do Spring Boot.
  */
 public interface SpringProfessorRepository extends 
 JpaRepository<ProfessorModel, Long> {
 
     /**
      * Busca um professor pela sua matrícula.
-     * @param registration Matrícula.
+     * @param registration Matrícula do professor.
      * @return Optional vazio ou ProfessorModel.
      */
     Optional<ProfessorModel> findByRegistration(String registration);
