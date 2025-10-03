49cefae4280d68c073cfb7ad1574420a3baaf4f2
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2025-10-03 01:49:07 -0300

    docs: interface do SpringCourseRepository foi re-documentada.

--- back-end/src/main/java/br/edu/com/tg/manager/infrastructure/persistence/mysql/repositories/SpringCourseRepository.java
+++ back-end/src/main/java/br/edu/com/tg/manager/infrastructure/persistence/mysql/repositories/SpringCourseRepository.java
@@ -1,23 +1,24 @@
 package br.edu.com.tg.manager.infrastructure.persistence.mysql.repositories;
 
 import br.edu.com.tg.manager.core.domain.enums.CourseShift;
-import br.edu.com.tg.manager.infrastructure.persistence.mysql.models.CourseModel;
+import br.edu.com.tg.manager.infrastructure.persistence.mysql.models
+        .CourseModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
-
 /**
- * Repositório Spring JPA.
- * Determina um contrato abstrato para o Spring JPA persistir e manipular o 
- * modelo de dados CourseModel.
+ * Repositório de fronteira:
+ * Define um contrato abstrato para o Spring JPA persistir e manipular o
+ * modelo de dados CourseModel. Por pertencer à infraestrutura da aplicação,
+ * esta classe herda o JPARepository do Spring Boot.
  */
 public interface SpringCourseRepository extends 
 JpaRepository<CourseModel, Long> {
     
     /**
-     * Busca um curso pelo seu nome e seu turno.
-     * @param name Nome.
-     * @param shift Turno.
+     * Busca um curso pelo seu nome e o seu turno.
+     * @param name Nome do curso.
+     * @param shift Turno do curso.
      * @return Optional vazio ou CourseModel.
      */
     Optional<CourseModel> findByNameAndShift(String name, CourseShift shift);
