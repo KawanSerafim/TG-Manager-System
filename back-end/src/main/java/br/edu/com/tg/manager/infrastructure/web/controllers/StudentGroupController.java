cdf393656df424cf277cf366af45d56d0b6153ef
Kawan Serafim <149839735+KawanSerafim@users.noreply.github.com>
2025-10-04 10:54:17 -0300

    docs: classe do StudentGroupController foi re-documentada.

--- back-end/src/main/java/br/edu/com/tg/manager/infrastructure/web/controllers/StudentGroupController.java
+++ back-end/src/main/java/br/edu/com/tg/manager/infrastructure/web/controllers/StudentGroupController.java
@@ -14,17 +14,47 @@ import org.springframework.web.multipart.MultipartFile;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/*
+ * Anotações da web.bind:
+ *
+ * Anotação @PostMapping: forma encurtada de um RequestMapping para método POST.
+ *
+ * Anotação @RequestMapping: indica ao Spring Boot o mapeamento das requisições.
+ * São dois níveis, o de classe que define o prefixo da URL, e o de método que
+ * define o endpoint.
+ *
+ * Anotação @RequestParam: indica ao Spring Boot o parâmetro que deve ser
+ * buscado na requisição, podendo extraí-los à uma variável.
+ *
+ * Anotação @RequestPart: indica ao Spring Boot o parâmetro que deve ser
+ * buscado na requisição, onde o parâmetro é um upload de arquivo.
+ *
+ * Anotação @RestController: indica ao Spring Boot que esta classe irá receber
+ * requisições web e que seus retornos serão sem modelo - geralmente, JSONs.
+ */
+
+/**
+ * Controlador de fronteira:
+ * Estabelece a comunicação com a página HTTP que envia os dados da turma. Por
+ * pertencer à infraestrutura da aplicação, esta classe utiliza das anotações
+ * web.bind do Spring Boot.
+ */
 @RestController
 @RequestMapping("/student-group/api")
 public class StudentGroupController {
 
     private static final Logger logger = LoggerFactory
         .getLogger(StudentGroupController.class);
-    private final CreateStudentGroupCase usecase;
+    private final CreateStudentGroupCase useCase;
 
-    public StudentGroupController(CreateStudentGroupCase usecase) {
+    /**
+     * Construtor de injeção de dependência:
+     *
+     * @param useCase Caso de uso de criar turma.
+     */
+    public StudentGroupController(CreateStudentGroupCase useCase) {
 
-        this.usecase = usecase;
+        this.useCase = useCase;
     }
 
     @PostMapping(consumes = "multipart/form-data")
@@ -38,6 +68,10 @@ public class StudentGroupController {
 
         try {
 
+            /*
+             * Atribuindo ao porta-dados do caso de uso, os dados recolhidos da
+             * requisição POST.
+             */
             var input = new CreateStudentGroupCase.Input(
 
                 courseName,
@@ -46,17 +80,28 @@ public class StudentGroupController {
                 temporaryPassword
             );
 
-            usecase.execute(input);
+            /*
+             * Executando o caso de uso. Por injeção de dependência, quem irá
+             * aplicar a lógica será o service responsável pelo caso de uso.
+             */
+            useCase.execute(input);
 
+            // Retorna ao cliente um body informando o sucesso da execução.
             return ResponseEntity.status(HttpStatus.CREATED)
                 .body("Turma e alunos pré-cadastrados com sucesso!");
         } catch (DomainException e) {
-            
+
+            /*
+             * Qualquer exceção de domínio lançado nas camadas internas, são
+             * tratadas aqui.
+             */
             return ResponseEntity.badRequest().body(e.getMessage());
         } catch (Exception e) {
 
+            // Logger para lançar o corpo da mensagem da exceção só no terminal.
             logger.error("Ocorreu um erro inesperado no servidor.", e);
 
+            // Retorna ao cliente um body informando erro no servidor.
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(
                     
