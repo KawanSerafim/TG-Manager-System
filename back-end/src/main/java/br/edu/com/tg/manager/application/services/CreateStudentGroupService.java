49545b6a2c175f98ed2b74e455bca9a587cce16b
Kawan Serafim <149839735+KawanSerafim@users.noreply.github.com>
2025-10-04 11:19:23 -0300

    docs: classe do CreateStudentGroupService foi re-documentada.

--- back-end/src/main/java/br/edu/com/tg/manager/application/services/CreateStudentGroupService.java
+++ back-end/src/main/java/br/edu/com/tg/manager/application/services/CreateStudentGroupService.java
@@ -12,6 +12,14 @@ import br.edu.com.tg.manager.core.usecases.CreateStudentGroupCase;
 import java.util.Optional;
 import org.springframework.stereotype.Service;
 
+/**
+ * Implementador de caso de uso:
+ * Implementa o funcionamento e a lógica do caso de uso de criar uma turma e
+ * a persistir no banco de dados. Por pertencer à infraestrutura da aplicação,
+ * esta classe utiliza da anotação Service do SpringBoot, que é uma
+ * especialização da anotação Component, permitindo que o framework gerencie
+ * a classe.
+ */
 @Service
 public class CreateStudentGroupService implements CreateStudentGroupCase {
 
@@ -21,14 +29,13 @@ public class CreateStudentGroupService implements CreateStudentGroupCase {
     private final StudentDataReader studentDataReader;
 
     /**
-     * Construtor de injeção de dependência.
-     * Injeta a dependência de quem for instanciar o caso de uso:
-     * CreateStudentGroupCase
+     * Construtor de injeção de dependência:
+     *
      * @param courseRepository Repositório de domínio do curso.
      * @param studentGroupRepository Repositório de domínio da turma.
      * @param studentRepository Repositório de domínio do aluno.
-     * @param studentDataReader Contrato para ler dados de aluno e demais
-     * informações de turma, vindos de um arquivo.
+     * @param studentDataReader Portão de acesso de domínio dos dados dos alunos
+     * e demais informações da turma.
      */
     public CreateStudentGroupService(
 
@@ -50,27 +57,30 @@ public class CreateStudentGroupService implements CreateStudentGroupCase {
     @Override
     public void execute(Input input) {
 
-        /* Salva numa instância, um record que traduz os dados vindo
+        /*
+         * Salva numa instância um record que traduz os dados vindo
          * do arquivo.
-        */
+         */
         var fileData = studentDataReader.read(input.file());
 
-        /* Salva numa instância, o curso que achar com o nome e o turno
+        /*
+         * Salva numa instância o curso que achar com o nome e o turno
          * fornecido pelo coordenador de TG. Se não achar, sobe um
          * DomainException de entidade não encontrada.
-        */
+         */
         Course course = courseRepository.findByNameAndShift(
 
             input.courseName(),
             fileData.shift()
         ).orElseThrow(() -> new DomainException(
-            
+
             "O curso com nome = " + input.courseName() + ", turno = " +
             fileData.shift() + " não foi encontrado."
         ));
         
         StudentGroup studentGroup;
-        
+
+        // Insere num Optional, a turma que será buscada pelo repositório.
         Optional<StudentGroup> optionalGroup = studentGroupRepository
             .findByCourseAndYearAndSemester(
                 course,
@@ -78,15 +88,19 @@ public class CreateStudentGroupService implements CreateStudentGroupCase {
                 fileData.semester()
             );
 
+        /*
+         * Verifica se a turma com o curso, ano e semestre fornecido já existe
+         * no banco de dados. Caso exista, reutiliza a mesma turma.
+         */
         if(optionalGroup.isPresent()) {
 
-            /* Se já existe aquela turma, cria um objeto para
-             * poder atualizá-la
-            */
             studentGroup = optionalGroup.get();
         } else {
 
-            /* Cria um objeto de turma com os dados capturados. */
+            /*
+             * Se não, cria uma nova turma, atribui nela os valores capturados
+             * pela requisição, e o salva no banco de dados.
+             */
             var newStudentGroup = new StudentGroup(
 
                 course,
@@ -101,14 +115,14 @@ public class CreateStudentGroupService implements CreateStudentGroupCase {
 
         for(var students : fileData.students()) {
 
-            /* Insere num Optional de Student, buscando o objeto pelo método de
-             * busca do repositório.
-             */
+
+            // Insere num Optional, o aluno que será buscado pelo repositório.
             Optional<Student> optionalStudent = studentRepository
             .findByRegistration(students.registration());
 
-            /* Verifica se o Optional retornou vazio. Se sim, o Student é
-             * persistido no banco.
+            /*
+             * Se o Optional for vazio, um novo aluno é criado e salvo no banco
+             * de dados. Isso garante que não haja alunos duplicados no sistema.
              */
             if(optionalStudent.isEmpty()) {
 
