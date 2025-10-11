26e04c8dc796e42810cdf0b6f362bf6b22901bcd
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2025-10-11 20:52:28 -0300

    docs: adicionado a documentação no record de infra da turma.

--- back-end/src/main/java/br/edu/com/tg/manager/infrastructure/web/dto/StudentGroupDataRequest.java
+++ back-end/src/main/java/br/edu/com/tg/manager/infrastructure/web/dto/StudentGroupDataRequest.java
@@ -4,6 +4,16 @@ import br.edu.com.tg.manager.core.domain.enums.CourseShift;
 import br.edu.com.tg.manager.core.domain.enums.Discipline;
 import java.util.List;
 
+/**
+ * Porta-dados de infra estrutura:
+ * Carregas os dados da turma que estão numa requisição HTTP.
+ * @param courseName Nome do curso da turma.
+ * @param discipline Disciplina da turma.
+ * @param year Ana da turma.
+ * @param semester Semestre da turma.
+ * @param courseShift Turno da turma.
+ * @param students Lista de alunos da turma.
+ */
 public record StudentGroupDataRequest(
 
         String courseName,
