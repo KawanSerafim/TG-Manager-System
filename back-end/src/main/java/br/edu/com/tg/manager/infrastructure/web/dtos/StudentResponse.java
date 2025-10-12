485cd2066944c13507c990c7e8f82b87ae8c36a6
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2025-10-11 23:29:42 -0300

    docs: comentário.

--- back-end/src/main/java/br/edu/com/tg/manager/infrastructure/web/dtos/StudentResponse.java
+++ back-end/src/main/java/br/edu/com/tg/manager/infrastructure/web/dtos/StudentResponse.java
@@ -1,5 +1,10 @@
 package br.edu.com.tg.manager.infrastructure.web.dtos;
 
+/**
+ * Porta-dados de infra estrutura:
+ * Carrega os dados de aluno que estavam no arquivo que o Coordenador de TG
+ * enviou.
+ */
 public record StudentResponse(
 
     String name,
