60b483d18c070c8f8f4437450c34a2b2008d61aa
Kawan Serafim <149839735+KawanSerafim@users.noreply.github.com>
2025-10-18 09:00:02 -0300

    refactor: refatorado as querys do sql.

--- back-end/src/main/resources/data.sql
+++ back-end/src/main/resources/data.sql
@@ -1,9 +1,9 @@
-INSERT IGNORE INTO professors(name, registration, email, hashed_password, role)
+INSERT IGNORE INTO professors(name, registration, email, password, role)
 VALUES
 ('Luciano', '11111111', 'luciano@fatec.sp.gov.br', 'senha123', 'COURSE_COORDINATOR'),
 ('Cristina', '22222222', 'cristina@fatec.sp.gov.br', 'senha123', 'TG_COORDINATOR');
 
 /*---------------------------------*/
 
-INSERT IGNORE INTO courses(name, shift, tg_coordinator_id, course_coordinator_id) 
+INSERT IGNORE INTO courses(name, shift, tg_coordinator_id, course_coordinator_id)
 VALUES('ADS - Análise e Desenvolvimento de Sistemas', 'NIGHT', 2, 1);
