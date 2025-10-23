0c231f0c21ef75087827bcf25d3d61aee7d9cc47
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2025-10-23 14:27:17 -0300

    feat: foi melhor tratadas as exceções na camada de fronteira.

--- back-end/src/main/java/br/edu/com/tg/manager/infrastructure/web/controllers/ProfessorController.java
+++ back-end/src/main/java/br/edu/com/tg/manager/infrastructure/web/controllers/ProfessorController.java
@@ -1,11 +1,8 @@
 package br.edu.com.tg.manager.infrastructure.web.controllers;
 
-import br.edu.com.tg.manager.core.domain.exceptions.DomainException;
 import br.edu.com.tg.manager.core.usecases.CreateProfessorCase;
 import br.edu.com.tg.manager.infrastructure.web.dtos.requests.ProfessorRequest;
 import br.edu.com.tg.manager.infrastructure.web.dtos.responses.ProfessorResponse;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,8 +12,6 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin(origins = "*")
 public class ProfessorController {
 
-    private static final Logger log = LoggerFactory
-        .getLogger(ProfessorController.class);
     private final CreateProfessorCase useCase;
 
     public ProfessorController(CreateProfessorCase useCase) {
@@ -32,60 +27,39 @@ public class ProfessorController {
      * tipo texto para DomainException ou Exception genérica.
      */
     @PostMapping("create-professor")
-    public ResponseEntity<?> create(@RequestBody ProfessorRequest request) {
+    public ResponseEntity<ProfessorResponse>
+    create(@RequestBody ProfessorRequest request) {
 
-        try {
+        /*
+         * Atribuindo ao porta-dados do caso de uso, os dados recolhidos da
+         * requisição POST.
+         */
+        var input = new CreateProfessorCase.Input(
 
-            /*
-             * Atribuindo ao porta-dados do caso de uso, os dados recolhidos da
-             * requisição POST.
-             */
-            var input = new CreateProfessorCase.Input(
+            request.name(),
+            request.registration(),
+            request.email(),
+            request.password(),
+            request.role()
+        );
 
-                request.name(),
-                request.registration(),
-                request.email(),
-                request.password(),
-                request.role()
-            );
+        /*
+         * Executando o caso de uso. Por injeção de dependência, quem irá
+         * aplicar a lógica será o service responsável pelo caso de uso.
+         */
+        var result = useCase.execute(input);
 
-            /*
-             * Executando o caso de uso. Por injeção de dependência, quem irá
-             * aplicar a lógica será o service responsável pelo caso de uso.
-             */
-            var result = useCase.execute(input);
+        // Monta um DTO de resposta com todos os dados
+        var responseBody = new ProfessorResponse(
 
-            // Monta um DTO de resposta com todos os dados
-            var responseBody = new ProfessorResponse(
+            result.id(),
+            result.name(),
+            result.registration(),
+            result.email(),
+            result.role()
+        );
 
-                result.id(),
-                result.name(),
-                result.registration(),
-                result.email(),
-                result.role()
-            );
-
-            // Retorna ao cliente o response body montado.
-            return ResponseEntity.status(HttpStatus.CREATED).body(responseBody);
-        } catch (DomainException e) {
-
-            /*
-             * Qualquer exceção de domínio lançado nas camadas internas, são
-             * tratadas aqui.
-             */
-            return ResponseEntity.badRequest().body(e.getMessage());
-        } catch (Exception e) {
-
-            // Logger para lançar o corpo da mensagem da exceção só no terminal.
-            log.error("Ocorreu um erro inesperado no servidor.", e);
-
-            // Retorna ao cliente um body informando erro no servidor.
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(
-
-                    "Ocorreu um erro inesperado no servidor. Por favor, " +
-                    "tente novamente mais tarde."
-                );
-        }
+        // Retorna ao cliente o response body montado.
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseBody);
     }
 }
--- back-end/src/main/java/br/edu/com/tg/manager/infrastructure/web/controllers/StudentGroupController.java
+++ back-end/src/main/java/br/edu/com/tg/manager/infrastructure/web/controllers/StudentGroupController.java
@@ -1,17 +1,16 @@
 package br.edu.com.tg.manager.infrastructure.web.controllers;
 
-import br.edu.com.tg.manager.core.domain.exceptions.DomainException;
 import br.edu.com.tg.manager.core.ports.gateways.StudentDataReader;
 import br.edu.com.tg.manager.core.usecases.CreateStudentGroupCase;
-import br.edu.com.tg.manager.infrastructure.web.dtos.requests.StudentGroupRequest;
-import br.edu.com.tg.manager.infrastructure.web.dtos.responses.StudentGroupResponse;
+import br.edu.com.tg.manager.infrastructure.web.dtos.requests
+        .StudentGroupRequest;
+import br.edu.com.tg.manager.infrastructure.web.dtos.responses
+        .StudentGroupResponse;
 import br.edu.com.tg.manager.infrastructure.web.dtos.responses.StudentResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -19,8 +18,6 @@ import java.util.List;
 @CrossOrigin(origins = "*")
 public class StudentGroupController {
 
-    private static final Logger logger = LoggerFactory
-        .getLogger(StudentGroupController.class);
     private final CreateStudentGroupCase useCase;
     private final StudentDataReader studentDataReader;
 
@@ -50,77 +47,56 @@ public class StudentGroupController {
      * tipo texto para DomainException ou Exception genérica.
      */
     @PostMapping(consumes = "multipart/form-data")
-    public ResponseEntity<?> create(@RequestBody StudentGroupRequest request) {
+    public ResponseEntity<StudentGroupResponse>
+    create(@RequestBody StudentGroupRequest request) throws IOException {
 
-        try {
+        /*
+         * Salva numa instância um record que traduz os dados vindo
+         * do arquivo.
+         */
+        var fileData = studentDataReader.read(request.inputStream());
 
-            /*
-             * Salva numa instância um record que traduz os dados vindo
-             * do arquivo.
-             */
-            var fileData = studentDataReader.read(request.inputStream());
+        /*
+         * Atribuindo ao porta-dados do caso de uso, os dados recolhidos da
+         * requisição POST.
+         */
+        var input = new CreateStudentGroupCase.Input(
 
-            /*
-             * Atribuindo ao porta-dados do caso de uso, os dados recolhidos da
-             * requisição POST.
-             */
-            var input = new CreateStudentGroupCase.Input(
+            request.courseName(),
+            request.discipline(),
+            fileData
+        );
 
-                request.courseName(),
-                request.discipline(),
-                fileData
-            );
+        /*
+         * Executando o caso de uso. Por injeção de dependência, quem irá
+         * aplicar a lógica será o service responsável pelo caso de uso.
+         */
+        var result = useCase.execute(input);
 
-            /*
-             * Executando o caso de uso. Por injeção de dependência, quem irá
-             * aplicar a lógica será o service responsável pelo caso de uso.
-             */
-            var result = useCase.execute(input);
+        // Converte a lista de alunos para um DTO.
+        List<StudentResponse> studentDTOs = result.students().stream()
+            .map(student ->
 
-            // Converte a lista de alunos para um DTO.
-            List<StudentResponse> studentDTOs = result.students().stream()
-                .map(student ->
+                new StudentResponse(
 
-                    new StudentResponse(
+                    student.getName(),
+                    student.getRegistration()
+                )
+            ).toList();
 
-                        student.getName(),
-                        student.getRegistration()
-                    )
-                ).toList();
+        // Monta um DTO de resposta com todos os dados
+        var responseBody = new StudentGroupResponse(
 
-            // Monta um DTO de resposta com todos os dados
-            var responseBody = new StudentGroupResponse(
+            result.courseName(),
+            result.courseShift(),
+            result.discipline(),
+            result.year(),
+            result.semester(),
+            studentDTOs
+        );
 
-                result.courseName(),
-                result.courseShift(),
-                result.discipline(),
-                result.year(),
-                result.semester(),
-                studentDTOs
-            );
-
-            // Retorna ao cliente o response body montado.
-            return ResponseEntity.status(HttpStatus.CREATED)
-                .body(responseBody);
-        } catch (DomainException e) {
-
-            /*
-             * Qualquer exceção de domínio lançado nas camadas internas, são
-             * tratadas aqui.
-             */
-            return ResponseEntity.badRequest().body(e.getMessage());
-        } catch (Exception e) {
-
-            // Logger para lançar o corpo da mensagem da exceção só no terminal.
-            logger.error("Ocorreu um erro inesperado no servidor.", e);
-
-            // Retorna ao cliente um body informando erro no servidor.
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(
-                    
-                    "Ocorreu um erro inesperado no servidor. Por favor, " +
-                    "tente novamente mais tarde."
-                );
-        }
+        // Retorna ao cliente o response body montado.
+        return ResponseEntity.status(HttpStatus.CREATED)
+            .body(responseBody);
     }
 }
