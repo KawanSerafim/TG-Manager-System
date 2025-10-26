9a9be2ea4604a495acf1764f8ec27e0dd9c2b5c6
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2025-10-26 01:52:24 -0300

    docs: a entidade de domínio Student está com comentários atualizados.

--- back-end/src/main/java/br/edu/com/tg/manager/core/domain/entities/Student.java
+++ back-end/src/main/java/br/edu/com/tg/manager/core/domain/entities/Student.java
@@ -1,18 +1,16 @@
 package br.edu.com.tg.manager.core.domain.entities;
 
 import br.edu.com.tg.manager.core.domain.enums.StudentStatus;
-import br.edu.com.tg.manager.core.domain.enums.UserAccountStatus;
 import br.edu.com.tg.manager.core.domain.exceptions.DomainException;
 
 /**
  * Entidade de domínio:
  * Representa um aluno da instituição, numa determinada turma.
- * Por pertencer ao núcleo (core) da aplicação, esta classe é independente de
- * frameworks ou bibliotecas externas, sendo, portanto, considerada uma
- * classe pura.
+ * Por pertencer ao núcleo (core) da aplicação, esta classe é
+ * independente de frameworks ou bibliotecas externas, sendo,
+ * portanto, considerada uma classe pura.
  */
 public class Student {
-
     private Long id;
     private String name;
     private String registration;
@@ -27,128 +25,114 @@ public class Student {
     public Student() {}
 
     /**
-     * Construtor de negócio:
-     * Cria um novo objeto de pre-Student e garante que o objeto seja criado num
-     * estado válido.
+     * Construtor de domínio:
+     * Cria um novo objeto de Student e garante que o objeto seja
+     * criado num estado inicial válido (PRE_REGISTRATION).
      * @param name Nome do aluno.
      * @param registration Matrícula do aluno.
      * @param studentGroup Turma do aluno.
      */
     public Student(
-
-        String name,
-        String registration,
-        StudentGroup studentGroup
+            String name,
+            String registration,
+            StudentGroup studentGroup
     ) {
+        // Define o status inicial no construtor de domínio.
+        this.setStatus(StudentStatus.PRE_REGISTRATION);
 
-        // Delega as validações dos parâmetros aos seus devidos métodos Set.
+        // Delega as validações dos parâmetros aos seus devidos setters.
         this.setName(name);
         this.setRegistration(registration);
         this.setStudentGroup(studentGroup);
-        this.setStatus(StudentStatus.PRE_REGISTRATION);
     }
 
-    /**
-     * Método Get.
-     * @return ID do aluno.
-     */
+    // MÉTODOS GETTERS E SETTERS.
+
     public Long getId() {
-     
         return id;
     }
 
-    /**
-     * Método Set.
-     * @param id ID fornecido.
-     */
     public void setId(Long id) {
-     
         this.id = id;
     }
 
-    /**
-     * Método Get.
-     * @return Nome do aluno.
-     */
     public String getName() {
-     
         return name;
     }
 
-    /**
-     * Método Set.
-     * @param name Nome fornecido.
-     */
     public void setName(String name) {
-        
         // Regra de negócio: aluno não pode conter nome vazio ou nulo.
         if(name == null || name.trim().isEmpty()) {
-
             throw new DomainException(
-
-                "O campo nome é obrigatório."
+                    "O campo nome é obrigatório."
             );
         }
-
         this.name = name;
     }
 
-    /**
-     * Método Get.
-     * @return Matrícula do aluno.
-     */
     public String getRegistration() {
-     
         return registration;
     }
 
-    /**
-     * Método Set.
-     * @param registration Matrícula fornecida.
-     */
     public void setRegistration(String registration) {
-        
         // Regra de negócio: aluno não pode conter matrícula vazia ou nula.
         if(registration == null || registration.trim().isEmpty()) {
-
             throw new DomainException(
-
-                "O campo matrícula é obrigatório."
+                    "O campo matrícula é obrigatório."
             );
         }
-
         this.registration = registration;
     }
 
-    /**
-     * Método Get.
-     * @return Conta de usuário do aluno.
-     */
     public UserAccount getUserAccount() {
-
         return userAccount;
     }
 
-    /**
-     * Método Set.
-     * @param userAccount Conta de usuário fornecida.
-     */
     public void setUserAccount(UserAccount userAccount) {
-
         this.userAccount = userAccount;
     }
 
+    public StudentStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(StudentStatus status) {
+        // Regra de negócio: aluno não pode conter status nulo.
+        if(status == null) {
+            throw new DomainException(
+                    "O campo status é obrigatório."
+            );
+        }
+        this.status = status;
+    }
+
+    public StudentGroup getStudentGroup() {
+        return studentGroup;
+    }
+
+    public void setStudentGroup(StudentGroup studentGroup) {
+        // Regra de negócio: aluno não pode conter turma nula.
+        if(studentGroup == null) {
+            throw new DomainException(
+                    "O campo turma é obrigatório."
+            );
+        }
+        this.studentGroup = studentGroup;
+    }
+
+    // MÉTODOS DE DELEGAÇÃO.
+
     /**
-     * Método Get.
-     * @return Email da conta do aluno.
+     * Método Get (DELEGAÇÃO).
+     * @return Email da conta de usuário do aluno.
      */
     public String getEmail() {
-     
+
         return userAccount.getEmail();
     }
 
     /**
-     * Método Set.
+     * Método Set (DELEGAÇÃO).
      * @param email Email fornecido.
      */
     public void setEmail(String email) {
@@ -157,74 +141,20 @@ public class Student {
     }
 
     /**
-     * Método Get.
-     * @return Senha criptografada da conta do aluno.
+     * Método Get (DELEGAÇÃO).
+     * @return Senha criptografada da conta de usuário do aluno.
      */
     public String getPassword() {
-     
+
         return userAccount.getPassword();
     }
 
     /**
-     * Método Set.
+     * Método Set (DELEGAÇÃO).
      * @param password Senha fornecida.
      */
     public void setPassword(String password) {
-     
+
         this.userAccount.setPassword(password);
-    }
-
-    /**
-     * Método Get.
-     * @return Status da conta do aluno.
-     */
-    public StudentStatus getStatus() {
-     
-        return status;
-    }
-
-    /**
-     * Método Set.
-     * @param status Status fornecido.
-     */
-    public void setStatus(StudentStatus status) {
-        
-        // Regra de negócio: aluno não pode conter status nulo.
-        if(status == null) {
-
-            throw new DomainException(
-
-                "O campo status é obrigatório."
-            );
-        }
-
-        this.status = status;
-    }
-
-    /**
-     * Método Get.
-     * @return Turma do aluno.
-     */
-    public StudentGroup getStudentGroup() {
-     
-        return studentGroup;
-    }
-
-    /**
-     * Método Set.
-     * @param studentGroup Turma fornecida.
-     */
-    public void setStudentGroup(StudentGroup studentGroup) {
-        
-        // Regra de negócio: aluno não pode conter turma nula.
-        if(studentGroup == null) {
-
-            throw new DomainException(
-
-                "O campo turma é obrigatório."
-            );
-        }
-
-        this.studentGroup = studentGroup;
     }
 }
