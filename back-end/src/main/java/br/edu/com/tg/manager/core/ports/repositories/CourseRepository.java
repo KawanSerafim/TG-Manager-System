180e3efd9e4bdf5b58a41428f1de43936bedf1dd
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2025-10-27 00:41:08 -0300

    docs: os repositórios de núcleo estão com comentários atualizados.

--- back-end/src/main/java/br/edu/com/tg/manager/core/ports/repositories/CourseRepository.java
+++ back-end/src/main/java/br/edu/com/tg/manager/core/ports/repositories/CourseRepository.java
@@ -5,24 +5,24 @@ import br.edu.com.tg.manager.core.domain.enums.CourseShift;
 import java.util.Optional;
 
 /**
- * Repositório de domínio:
- * Define um contrato abstrato para a camada de infraestrutura implementar
- * a persistência de dados da entidade de domínio Course.
- * Por pertencer ao núcleo (core) da aplicação, esta interface é independente de
- * frameworks ou bibliotecas externas, sendo, portanto, considerada uma
- * interface pura.
+ * Repositório de núcleo:
+ * Define um contrato abstrato para a camada de infraestrutura
+ * implementar a persistência de dados da entidade de domínio Course.
+ * Por pertencer ao núcleo (core) da aplicação, esta interface é
+ * independente de frameworks ou bibliotecas externas, sendo,
+ * portanto, considerada uma interface pura.
  */
 public interface CourseRepository {
-
     /**
-     * Método de contrato de domínio:
+     * Método de contrato de núcleo:
      * Persiste o objeto de um curso no banco de dados.
      * @param course Curso.
+     * @return Course.
      */
     Course save(Course course);
 
     /**
-     * Método de contrato de domínio:
+     * Método de contrato de núcleo:
      * Busca um curso pelo seu nome e seu turno.
      * @param name Nome do curso.
      * @param shift Turno do curso.
--- back-end/src/main/java/br/edu/com/tg/manager/core/ports/repositories/StudentRepository.java
+++ back-end/src/main/java/br/edu/com/tg/manager/core/ports/repositories/StudentRepository.java
@@ -4,32 +4,31 @@ import br.edu.com.tg.manager.core.domain.entities.Student;
 import java.util.Optional;
 
 /**
- * Repositório de domínio:
- * Determina um contrato abstrato para a camada de infraestrutura implementar
- * a persistência de dados da entidade de domínio Student.
- * Por pertencer ao núcleo (core) da aplicação, esta interface é independente de
- * frameworks ou bibliotecas externas, sendo, portanto, considerada uma
- * interface pura.
+ * Repositório de núcleo:
+ * Define um contrato abstrato para a camada de infraestrutura
+ * implementar a persistência de dados da entidade de domínio Student.
+ * Por pertencer ao núcleo (core) da aplicação, esta interface é
+ * independente de frameworks ou bibliotecas externas, sendo,
+ * portanto, considerada uma interface pura.
  */
 public interface StudentRepository {
-
     /**
-     * Método de contrato de domínio:
+     * Método de contrato de núcleo:
      * Persiste o objeto de um aluno no banco de dados.
      * @param student Aluno.
      */
     void save(Student student);
 
     /**
-     * Método de contrato de domínio:
+     * Método de contrato de núcleo:
      * Busca um aluno pela sua matrícula.
-     * @param registration RA do aluno.
+     * @param registration Matrícula do Aluno.
      * @return Optional vazio ou Student.
      */
     Optional<Student> findByRegistration(String registration);
 
     /**
-     * Método de contrato de domínio:
+     * Método de contrato de núcleo:
      * Busca um aluno pelo seu email.
      * @param email Email do aluno.
      * @return Optional vazio ou Student.
