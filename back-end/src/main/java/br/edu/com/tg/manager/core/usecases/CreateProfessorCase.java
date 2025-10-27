4a803a865733e67a1ea74b312a65be130d931e56
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2025-10-27 01:32:37 -0300

    docs: os casos de uso de núcleo estão com comentários atualizados.

--- back-end/src/main/java/br/edu/com/tg/manager/core/usecases/CreateProfessorCase.java
+++ back-end/src/main/java/br/edu/com/tg/manager/core/usecases/CreateProfessorCase.java
@@ -1,58 +1,55 @@
 package br.edu.com.tg.manager.core.usecases;
 
-import br.edu.com.tg.manager.core.domain.entities.UserAccount;
 import br.edu.com.tg.manager.core.domain.enums.ProfessorRole;
 
 /**
- * Caso de uso de domínio:
- * Define um contrato abstrato para a camada de aplicação implementar a
- * lógica de criar um professor.
- * Por pertencer ao núcleo (core) da aplicação, esta interface é independente de
- * frameworks ou bibliotecas externas, sendo, portanto, considerada uma
- * interface pura.
+ * Caso de uso de núcleo:
+ * Define um contrato abstrato para a camada de aplicação
+ * implementar a lógica de cadastrar um professor.
+ * Por pertencer ao núcleo (core) da aplicação, esta interface é
+ * independente de frameworks ou bibliotecas externas, sendo,
+ * portanto, considerada uma interface pura.
  */
 public interface CreateProfessorCase {
-
     /**
-     * Porta-dados de domínio:
-     * Carrega os dados recebidos pela requisição.
+     * Porta-dados de contrato de núcleo:
+     * Carrega os dados enviados pela requisição POST.
      * @param name Nome do professor.
-     * @param registration Matricula do professor.
+     * @param registration Matrícula do professor.
      * @param email Email do professor.
      * @param password Senha do professor.
      * @param role Cargo do professor.
      */
     record Input(
-
-        String name,
-        String registration,
-        String email,
-        String password,
-        ProfessorRole role
+            String name,
+            String registration,
+            String email,
+            String password,
+            ProfessorRole role
     ) {}
 
     /**
-     * Porta-dados de domínio:
-     * Carrega os dados recebidos pela requisição que foram salvos.
+     * Porta-dados de contrato de núcleo:
+     * Carrega os dados de informações dos coordenadores do curso.
+     * @param id ID do professor
      * @param name Nome do professor.
-     * @param registration Matricula do professor.
+     * @param registration Matrícula do professor.
      * @param email Email do professor.
      * @param role Cargo do professor.
      */
     record Output(
-
-        Long id,
-        String name,
-        String registration,
-        String email,
-        ProfessorRole role
+            Long id,
+            String name,
+            String registration,
+            String email,
+            ProfessorRole role
     ) {}
 
     /**
-     * Método de contrato de domínio:
-     * Executa a criação de um novo professor, e o persiste no banco de dados.
-     * @param input Porta-dados da requisição.
-     * @return Porta-dados da resposta da requisição.
+     * Método de contrato de núcleo:
+     * Executa o cadastro de um professor no sistema.
+     * @param input Porta-dados da requisição POST.
+     * @return Output.
      */
     Output execute(Input input);
 }
--- back-end/src/main/java/br/edu/com/tg/manager/core/usecases/SendConfirmationEmailCase.java
+++ back-end/src/main/java/br/edu/com/tg/manager/core/usecases/SendConfirmationEmailCase.java
@@ -1,26 +1,25 @@
 package br.edu.com.tg.manager.core.usecases;
 
 /**
- * Caso de uso de domínio:
- * Define um contrato abstrato para a camada de aplicação implementar a lógica
- * de enviar um email de confirmação.
- * Por pertencer ao núcleo (core) da aplicação, esta interface é independente de
- * frameworks ou bibliotecas externas, sendo, portanto, considerada uma
- * interface pura.
+ * Caso de uso de núcleo:
+ * Define um contrato abstrato para a camada de aplicação
+ * implementar a lógica de enviar um email de confirmação.
+ * Por pertencer ao núcleo (core) da aplicação, esta interface é
+ * independente de frameworks ou bibliotecas externas, sendo,
+ * portanto, considerada uma interface pura.
  */
 public interface SendConfirmationEmailCase {
-
     /**
-     * Porta-dados de domínio:
-     * Carrega os dados recebidos pela requisição.
-     * @param email Email do usuário.
+     * Porta-dados de contrato de núcleo:
+     * Carrega os dados enviados pela requisição POST.
+     * @param email Email.
      */
     record Input(String email) {}
 
     /**
-     * Método de contrato de domínio:
+     * Método de contrato de núcleo:
      * Executa o envio de email de confirmação.
-     * @param input Porta-dados da requisição.
+     * @param input Porta-dados da requisição POST.
      */
     void execute(Input input);
 }
