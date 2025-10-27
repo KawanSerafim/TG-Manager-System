7274fe6063df4c247f049a1ccfa6c2ccba89ec0d
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2025-10-27 16:28:49 -0300

    fix: agora os endpoints não estão mais bloqueados.

--- back-end/src/main/java/br/edu/com/tg/manager/infrastructure/configuration/SecurityConfig.java
+++ back-end/src/main/java/br/edu/com/tg/manager/infrastructure/configuration/SecurityConfig.java
@@ -2,13 +2,32 @@ package br.edu.com.tg.manager.infrastructure.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http)
+            throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/api/**").permitAll()
+                        .requestMatchers("/professors/api/**").permitAll()
+                        .requestMatchers("/courses/api/**").permitAll()
+                        .requestMatchers("/student-group/api/**").permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
     }
 }
