644d6f045488af80a9aa479a220a99680c15d570
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2025-10-30 16:43:22 -0300

    feat: caso de uso Listar Coordenadores de TG finalizado.

--- back-end/src/main/java/br/edu/com/tg/manager/core/usecases/ListTgCoordinatorsCase.java
+++ back-end/src/main/java/br/edu/com/tg/manager/core/usecases/ListTgCoordinatorsCase.java
@@ -1,4 +1,27 @@
 package br.edu.com.tg.manager.core.usecases;
 
+import java.util.List;
+
+/**
+ * Caso de uso de núcleo:
+ * Define um contrato abstrato para a camada de aplicação
+ * implementar a lógica de listar coordenadores de TG.
+ * Por pertencer ao núcleo (core) da aplicação, esta interface é
+ * independente de frameworks ou bibliotecas externas, sendo,
+ * portanto, considerada uma interface pura.
+ */
 public interface ListTgCoordinatorsCase {
+    /**
+     * Porta-dados de contrato de núcleo:
+     * Carrega os dados resposta a requisição POST.
+     * @param name Nome do coordenador de TG.
+     */
+    record Output(String name, String registration) {}
+
+    /**
+     * Método de contráto de núcleo:
+     * Executa a listagem de coordenadores de TG.
+     * @return Output.
+     */
+    List<Output> execute();
 }
--- back-end/src/main/java/br/edu/com/tg/manager/infrastructure/web/controllers/ProfessorController.java
+++ back-end/src/main/java/br/edu/com/tg/manager/infrastructure/web/controllers/ProfessorController.java
@@ -1,20 +1,35 @@
 package br.edu.com.tg.manager.infrastructure.web.controllers;
 
 import br.edu.com.tg.manager.core.usecases.CreateProfessorCase;
+import br.edu.com.tg.manager.core.usecases.ListTgCoordinatorsCase;
 import br.edu.com.tg.manager.infrastructure.web.dtos.requests.ProfessorRequest;
-import br.edu.com.tg.manager.infrastructure.web.dtos.responses.ProfessorResponse;
+import br.edu.com.tg.manager.infrastructure.web.dtos.responses.professor
+        .ListCoordinatorResponse;
+import br.edu.com.tg.manager.infrastructure.web.dtos.responses.professor
+        .ProfessorResponse;
+import br.edu.com.tg.manager.infrastructure.web.mappers
+        .ListCoordinatorResponseMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
 @RequestMapping("/professors/api")
 @CrossOrigin(origins = "*")
 public class ProfessorController {
-    private final CreateProfessorCase useCase;
+    private final CreateProfessorCase createUseCase;
+    private final ListTgCoordinatorsCase listTgUseCase;
+    private final ListCoordinatorResponseMapper mapper;
 
-    public ProfessorController(CreateProfessorCase useCase) {
-        this.useCase = useCase;
+    public ProfessorController(
+            CreateProfessorCase createUseCase,
+            ListTgCoordinatorsCase listTgUseCase,
+            ListCoordinatorResponseMapper mapper
+    ) {
+        this.createUseCase = createUseCase;
+        this.listTgUseCase = listTgUseCase;
+        this.mapper = mapper;
     }
 
     @PostMapping("create")
@@ -29,7 +44,7 @@ public class ProfessorController {
                 request.role()
         );
 
-        var result = useCase.execute(input);
+        var result = createUseCase.execute(input);
 
         var responseBody = new ProfessorResponse(
                 result.id(),
@@ -40,5 +55,14 @@ public class ProfessorController {
         );
 
         return ResponseEntity.status(HttpStatus.CREATED).body(responseBody);
+    }
+
+    @GetMapping("/list/tg-coordinators")
+    public ResponseEntity<List<ListCoordinatorResponse>> listTgCoordinators() {
+        var results = listTgUseCase.execute();
+
+        var responseBody = mapper.toResponseList(results);
+
+        return ResponseEntity.status(HttpStatus.OK).body(responseBody);
     }
 }
