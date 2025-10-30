a8d8857e3b15dc17891f71ae84925ff0bb3e26df
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2025-10-30 17:12:18 -0300

    feat: caso de uso Listar Coordenadores de Curso finalizado.

--- back-end/src/main/java/br/edu/com/tg/manager/infrastructure/web/mappers/ListCourseCoordinatorResponseMapper.java
+++ back-end/src/main/java/br/edu/com/tg/manager/infrastructure/web/mappers/ListCourseCoordinatorResponseMapper.java
@@ -1,4 +1,25 @@
 package br.edu.com.tg.manager.infrastructure.web.mappers;
 
+import br.edu.com.tg.manager.core.usecases.ListCourseCoordinatorsCase;
+import br.edu.com.tg.manager.infrastructure.web.dtos.responses.professor
+        .ListCoordinatorResponse;
+import org.springframework.stereotype.Component;
+import java.util.List;
+
+@Component
 public class ListCourseCoordinatorResponseMapper {
+    public ListCoordinatorResponse toResponse(
+            ListCourseCoordinatorsCase.Output result
+    ) {
+        return new ListCoordinatorResponse(
+                result.name(),
+                result.registration()
+        );
+    }
+
+    public List<ListCoordinatorResponse> toResponseList(
+            List<ListCourseCoordinatorsCase.Output> results
+    ) {
+        return results.stream().map(this::toResponse).toList();
+    }
 }
